8a391ac49392134677d228aa2d0e573778b8798c
Peter <schmidt.multimedia@gmail.com>
2023-07-20 09:20:27 +0200

    Lösungsvariante

--- script_01.js
+++ script_01.js
@@ -1,6 +1,7 @@
 
+
 /*** Mini-Challenge  */
-// Satzbau + Punktuation
+// Satzbau + variable Punktuation
 
 // Vereinbarung:
 // "S" (sentence) --> "."  Ich bin Max.
@@ -16,14 +17,33 @@ output(getSentence(["Ich","bin","Max"],"S"));
 output(getSentence(["Bist","du","Max"],"Q"));
 output(getSentence(["Ich","bin"],"E"));
 output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
-
 function getSentence(arr,op) {
-   
-   
-   
-     return "toDo";
+    const GAP = " ";
+    const PUNCT = getPunct(op);  // . ? !
+    let str = ""; 
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i != arr.length-1) {
+            str += arr[i] + GAP;
+        } else {
+            str += arr[i] + PUNCT; 
+        }
+    }
+     return str;
 }
 
+function getPunct(op) {
+    switch (op) {
+        case "S":
+           return "."; 
+        case "Q":
+            return "?";
+        case "E":
+            return "!";
+        default:
+            return "#";
+    }
+}
 
 
 // Modul: Ausgabe in Konsole : Test
